3519cea2791556e78d4e15a81042c046bb0d1cea
Adeena Lathiya <adeenalathiya@gmail.com>
2020-10-13 16:53:39 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Student from './Student.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+      <h1 className="leaders-board">Leaders Board</h1>
+        <Student name="John Doe" university="NED" score={347}/>
+        <Student name="Rizwan" university="NUST" score={345}/>
+        <Student name="Shahzaib" university="Uiversity Of Malaya" score={340}/>
+      </div>
+    );
+  }
 }
 
 export default App;
